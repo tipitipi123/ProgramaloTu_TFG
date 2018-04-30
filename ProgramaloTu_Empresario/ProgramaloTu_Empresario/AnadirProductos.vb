48dc4cad903c7b819eed833d44ebbd637608bcbf
Juan Antonio Sanchez Alarcon <tipix1998@gmail.com>
2018-04-30 16:31:22 +0200

    Solucion de problemas
    Se comprueban todos los datos en añadir producto excepto si precio es un double, mirar en internet

--- ProgramaloTu_Empresario/ProgramaloTu_Empresario/AnadirProductos.vb
+++ ProgramaloTu_Empresario/ProgramaloTu_Empresario/AnadirProductos.vb
@@ -2,6 +2,7 @@
 Imports System.IO
 Public Class AnadirProductos
     Dim foto As Byte()
+    Dim categoria As Integer
     '//////////////////////////////////////////////////////////////////
     'FUNCIÓN LOAD PARA CARGAR EN EL COMBOBOX LAS CATEGORIAS DISPONIBLES
     '//////////////////////////////////////////////////////////////////
@@ -50,7 +51,7 @@ Public Class AnadirProductos
                     pbImagen.Image = Image.FromFile(openFile.FileName)
                 End If
             Catch Ex As Exception
-                MessageBox.Show("NO SE PUEDE LEER LA IMAGEN: " & Ex.Message)
+                MessageBox.Show("NO SE PUEDE LEER LA IMAGEN")
             Finally
                 If (myStream IsNot Nothing) Then
                     myStream.Close()
@@ -64,12 +65,12 @@ Public Class AnadirProductos
     'BOTON PARA ALMACENAR LA INFORMACION EN LA BASE DE DATOS
     '////////////////////////////////////////////////////////
     Private Sub btnGuardar_Click(sender As Object, e As EventArgs) Handles btnGuardar.Click
-        guardar_producto()
+        If comprobar_todo() Then
+            guardar_producto()
+        End If
     End Sub
 
     Private Sub guardar_producto()
-        Dim id_categoria = devolver_id_categoria()
-
         Try
             conn.Open()
             Dim Query As String
@@ -77,7 +78,7 @@ Public Class AnadirProductos
             Command = New MySqlCommand(Query, conn)
             Command.Parameters.AddWithValue("?foto", foto)
             Command.Parameters.AddWithValue("?nombre", txtNombre.Text)
-            Command.Parameters.AddWithValue("?categoria", id_categoria)
+            Command.Parameters.AddWithValue("?categoria", categoria)
             Command.Parameters.AddWithValue("?precio", txtPrecio.Text)
 
             READER = Command.ExecuteReader
@@ -92,10 +93,10 @@ Public Class AnadirProductos
 
 
 
-    '///////////////////////////////////////////////////////
-    'FUNCIÓN QUE DEVUELVE EL ID DE LA CATEGORÍA SELECCIONADA
-    '///////////////////////////////////////////////////////
-    Private Function devolver_id_categoria()
+    '///////////////////////////////////////////////////////////////////////////////////
+    'FUNCIÓN QUE OBTIENE EL ID DE LA CATEGORÍA SELECCIONADA Y COMPRUEBA SI ES CORRECTA 
+    '//////////////////////////////////////////////////////////////////////////////////
+    Private Function obtener_id_categoria()
         Dim id As Integer = 0
         Dim Query As String
         Query = "SELECT id_categoria FROM Categorias where nombre='" & txtCategoria.Text & "'"
@@ -110,7 +111,8 @@ Public Class AnadirProductos
                 id = READER("id_categoria")
                 READER.Close()
                 conn.Close()
-                Return id
+                categoria = id
+                Return True
             End If
             READER.Close()
             conn.Close()
@@ -122,5 +124,40 @@ Public Class AnadirProductos
         Return False
     End Function
 
+    '///////////////////////////////////////////////////////////
+    'COMPROBAR SI LOS DATOS INTRODUCIDOS SON CORRECTOS
+    '///////////////////////////////////////////////////////////
+    Private Function comprobar_nombre()
+        If txtNombre.Text = "" Then
+            mostrar_error.SetError(Me.txtNombre, "TIENE QUE INTRODUCIR UN NOMBRE CORRECTO")
+            Return False
+        End If
+        mostrar_error.SetError(Me.txtNombre, "")
+        Return True
+    End Function
 
+    Private Function comprobar_precio()
+        If txtPrecio.Text = "" And Not Double.TryParse(txtPrecio.Text, 2) Then
+            mostrar_error.SetError(Me.txtPrecio, "TIENE QUE INTRODUCIR UN PRECIO CORRECTO")
+            Return False
+        End If
+        mostrar_error.SetError(Me.txtPrecio, "")
+        Return True
+    End Function
+
+    Private Function comprobar_imagen()
+        If IsNothing(foto) Then
+            mostrar_error.SetError(Me.pbImagen, "SELECCIONE UNA IMAGEN")
+            Return False
+        End If
+        mostrar_error.SetError(Me.pbImagen, "")
+        Return True
+    End Function
+
+    Private Function comprobar_todo()
+        If comprobar_nombre() And comprobar_precio() And comprobar_imagen() And obtener_id_categoria() Then
+            Return True
+        End If
+        Return False
+    End Function
 End Class
